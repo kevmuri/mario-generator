00244121481c737503ca8e858a1a8d9ce9f38779
kevmuri <kevmuri@umich.edu>
2024-07-11 15:03:23 -0400

    it works!!!

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ import mario_hairShirtBoots from './mario_hairShirtBoots.png';
 import mario_skin from './mario_skin.png';
 import JsBarcode from "jsbarcode";
 
-const colors = [
-  '#ffa347',
-  '#f83800',
-  '#af7f00',
-  '#0099ff',
-  '#00cc66',
-  '#ffffff',
-  '#000000'
-]
+const colors = {
+  orange: '#ffa347',
+  red: '#f83800',
+  brown: '#af7f00',
+  blue: '#0099ff',
+  green: '#00cc66',
+  white: '#ffffff',
+  black: '#000000'
+}
 
 function App() {
-  const [overallsColor, setOverallsColor] = useState(colors[1]);
-  const [hairShirtBootsColor, setHairShirtBootsColor] = useState(colors[2]);
-  const [skinColor, setSkinColor] = useState(colors[0]);
+  const [overallsColor, setOverallsColor] = useState(colors.red);
+  const [hairShirtBootsColor, setHairShirtBootsColor] = useState(colors.brown);
+  const [skinColor, setSkinColor] = useState(colors.orange);
 
   const [overallsImage, setOverallsImage] = useState(mario_overalls);
   const [hairShirtBootsImage, setHairShirtBootsImage] = useState(mario_hairShirtBoots);
@@ -32,41 +32,81 @@ function App() {
   const [imageData, setImageData] = useState('');
   const [barcodeData, setBarcodeData] = useState('0');
 
-  async function updateColors() {
-    setOverallsColor(colors[Math.floor(Math.random() * colors.length)])
+  async function updateColors(target) {
+    const new_color = randomColor();
 
-    replaceColor({
-      image: mario_overalls,
-      colors: {
-        type: 'hex',
-        targetColor: colors[1],
-        replaceColor: overallsColor
-      },
-      deltaE: 0
-    }).then((jimp_obj) => {
-      jimp_obj.getBase64(jimp_obj.getMIME(), (err, src) => {
-            setOverallsImage(src);
-          }
-      )
-    });
+    if (target === 'overalls') {
+      replaceColor({
+        image: mario_overalls,
+        colors: {
+          type: 'hex',
+          targetColor: colors.red,
+          replaceColor: new_color
+        },
+        deltaE: 1.0
+      }).then((jimp_obj) => {
+        jimp_obj.getBase64(jimp_obj.getMIME(), (err, src) => {
+              setOverallsImage(src);
+            }
+        )
+      })
 
-    mergeImages([overallsImage, hairShirtBootsImage, skinImage]).then(setImageData);
+      setOverallsColor(new_color);
+    }
+    else if (target === 'hairShirtBoots') {
+        replaceColor({
+            image: mario_hairShirtBoots,
+            colors: {
+            type: 'hex',
+            targetColor: colors.brown,
+            replaceColor: new_color
+            },
+            deltaE: 1.0
+        }).then((jimp_obj) => {
+            jimp_obj.getBase64(jimp_obj.getMIME(), (err, src) => {
+                setHairShirtBootsImage(src);
+                }
+            )
+        })
 
+        setHairShirtBootsColor(new_color);
+    }
+
+    else if (target === 'skin') {
+        replaceColor({
+            image: mario_skin,
+            colors: {
+            type: 'hex',
+            targetColor: colors.orange,
+            replaceColor: new_color
+            },
+            deltaE: 1.0
+        }).then((jimp_obj) => {
+            jimp_obj.getBase64(jimp_obj.getMIME(), (err, src) => {
+                setSkinImage(src);
+                }
+            )
+        })
+
+        setSkinColor(new_color);
+    }
   }
 
-  function updateBarcode() {
-    const barcodeString = `${colors.indexOf(overallsColor)}${colors.indexOf(hairShirtBootsColor)}${colors.indexOf(skinColor)}`
+
+  function updateBarcode(new_color) {
+    function getKeyByValue(object, value) {
+      return Object.keys(object).find(key => object[key] === value);
+    }
+
+    const barcodeString = `${getKeyByValue(colors, new_color)}${getKeyByValue(colors, hairShirtBootsColor)}${getKeyByValue(colors, skinColor)}`
     JsBarcode("#barcode", barcodeString)
+
+    console.log(barcodeString)
   }
 
-  useEffect(() => {
-    mergeImages([overallsImage, hairShirtBootsImage, skinImage]).then(setImageData);
-    updateBarcode();
-  }, [])
-
-  useEffect(() => {
-    updateBarcode();
-  }, [overallsColor, hairShirtBootsColor, skinColor])
+  function randomColor() {
+      return Object.values(colors)[Math.floor(Math.random() * Object.keys(colors).length)];
+  }
 
   async function getBase64(image) {
     const response = await fetch(image);
@@ -80,10 +120,22 @@ function App() {
     });
   }
 
+  useEffect(() => {
+    mergeImages([overallsImage, hairShirtBootsImage, skinImage])
+  }, [])
+
+  useEffect(() => {
+    mergeImages([overallsImage, hairShirtBootsImage, skinImage]).then(setImageData)
+    updateBarcode(overallsColor)
+  }, [overallsImage, hairShirtBootsImage, skinImage])
+
   return (
       <div className="App">
-        <button onClick={() => updateColors()}>Change Overalls Color</button>
         <img src={imageData}/>
+        <div/>
+        <button onClick={() => updateColors('overalls')}>Change Overalls Color</button>
+        <button onClick={() => updateColors('hairShirtBoots')}>Change Hair, Shirt, Boots Color</button>
+        <button onClick={() => updateColors('skin')}>Change skin Color</button>
         <div/>
         <svg id="barcode"></svg>
       </div>
